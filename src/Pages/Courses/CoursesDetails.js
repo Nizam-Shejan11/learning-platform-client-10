81bb7b79a18fa6dc22d34ffbe40b2f07e9e65057
Nizam-Shejan11 <www.nizam.shejan11@gmail.com>
2022-10-26 17:29:33 +0600

    card added

--- src/Pages/Courses/CoursesDetails.js
+++ src/Pages/Courses/CoursesDetails.js
@@ -1,20 +1,25 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import "./CoursesDetails.css";
 
-const CoursesDetails = () => {
+const CoursesDetails = ({ course }) => {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <div>
+      <Card className=" w-50 mx-auto mt-5 mb-5 text-center">
+        <Card.Header>Featured</Card.Header>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Special title treatment</Card.Title>
+          <Card.Text>
+            With supporting text below as a natural lead-in to additional
+            content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+        <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      </Card>
+    </div>
   );
 };
 
